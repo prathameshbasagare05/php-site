eb53486737452031d53fe4c10ed918cbf026a151
Prathamesh Basagare <prathamesh.basagare@wisdmlabs.com>
2025-01-27 12:05:30 +0530

    Initial Commit

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Site</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <header class="navbar">
+    <div class="container">
+      <a href="#" class="logo">Home</a>
+      <a href="#" class="login-button">Login</a>
+    </div>
+  </header>
+  <main class="main-content">
+    <div class="content">
+    </div>
+  </main>
+</body>
+</html>
