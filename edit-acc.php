9caee31ab47c123915b950989933a1d59cf8c53d
Prathamesh Basagare <prathamesh.basagare@wisdmlabs.com>
2025-01-28 12:55:49 +0530

    Changed edit-acc.php

--- edit-acc.php
+++ edit-acc.php
@@ -29,6 +29,29 @@ if(!isset($_SESSION['username'])) header("location:home.php");
             $email = $row['EmailID'];
             $passwordHash = $row['PasswordHash'];
         ?>
-        
+        <main class="main-content">
+            <div class = "profile-fields">
+                <form class = "field" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+                    <h2 class="item">Username</h2>
+                    <input class="item" type="text" name="username" id="username" value="<?php echo $username ?>">
+                    <button class="item"  type="submit" name="submit-username">Change Username</button>
+                </form>
+                <form class = "field" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+                    <h2 class="item" >Name</h2>
+                    <input class="item"  type="text" name="name" id="name" value="<?php echo $name ?>">
+                    <button class="item"  type="submit" name="submit-name">Change Name</button>
+                </form>
+                <form class = "field" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+                    <h2 class="item" >Mobile Number</h2>  
+                    <input class="item"  type="text" name="mobile" id="mobile" value="<?php echo $mobile ?>">
+                    <button class="item"  type="submit" name="submit-mobile">Change Mobile Number</button>
+                </form>
+                <form class = "field" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+                    <h2 class="item" >Email</h2>
+                    <input class="item"  type="text" name="email" id="email" value="<?php echo $email ?>">
+                    <button class="item"  type="submit" name="submit-email">Change Email</button>
+                </form>
+            </div>
+        </main>
     </body>
 </html>
